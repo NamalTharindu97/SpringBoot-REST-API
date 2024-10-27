1006c7f4e9eea5287b55c6151aa7531157fe31ad
NamalTharindu97 <namal@Namals-MacBook-Pro.local>
2024-10-27 18:08:50 +0530

    spring commit one

---
+++ src/main/java/com/namal/roadtospringone/RoadToSpringOneApplication.java
@@ -0,0 +1,13 @@
+package com.namal.roadtospringone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoadToSpringOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoadToSpringOneApplication.class, args);
+    }
+
+}
